b26649a6f764c3aaecd0aa16985523223b0f2bec
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-01-06 17:28:58 -0600

    Migrated the codebase from common js to esm

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,7 +13,7 @@ function createWindow(): void {
     autoHideMenuBar: process.platform === 'win32' ? false : true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
+      preload: join(__dirname, '../preload/index.mjs'),
       sandbox: false
     }
   })
