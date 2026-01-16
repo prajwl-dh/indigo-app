8cedf998da68ab92fb0564b1951dc21170105e3f
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-01-15 22:50:03 -0600

    Changed db pointer to default for db path

--- src/renderer/src/components/workspace/Sidebar.tsx
+++ src/renderer/src/components/workspace/Sidebar.tsx
@@ -16,9 +16,9 @@ export default function Sidebar({ activeDatabase }: SidebarType): React.JSX.Elem
     return (
         <div className="w-70 lg:w-80 flex shrink-0 flex-col bg-light-foreground dark:bg-dark-foreground border-r border-light-border dark:border-dark-border transition-all duration-500 ease-[cubic-bezier(0.25,1,0.5,1)] overflow-hidden">
             {isSidebarOpen && (
-                <div className="flex gap-2 justify-start items-center px-2 py-1 border-b border-light-border dark:border-dark-border text-[10px] font-medium text-light-secondaryText dark:text-dark-secondaryText">
+                <div className="flex gap-2 justify-start items-center px-2 py-1 border-b border-light-border dark:border-dark-border text-[10px] font-medium text-light-secondaryText dark:text-dark-secondaryText bg-light-surface dark:bg-dark-surface">
                     <Database className="w-3 shrink-0" />
-                    <span>{activeDatabase}</span>
+                    <span className="cursor-default">{activeDatabase}</span>
                 </div>
             )}
             Sidebar
