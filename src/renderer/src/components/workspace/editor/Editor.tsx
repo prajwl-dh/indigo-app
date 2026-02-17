ba1a67c4dc2d11eadba92352e9dadbee49c38189
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-02-16 19:48:30 -0600

    Added note title and body to editor

--- src/renderer/src/components/workspace/editor/Editor.tsx
+++ src/renderer/src/components/workspace/editor/Editor.tsx
@@ -2,6 +2,7 @@ import NoteOptionsComponent from '@renderer/components/ui/NoteOptionsComponent'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 import React from 'react'
+import TextareaAutosize from 'react-textarea-autosize'
 import ReactTimeAgo from 'react-time-ago'
 import { Accent } from 'src/shared/model/accent'
 import { Folders, Note, Notes } from 'src/shared/model/note'
@@ -29,6 +30,8 @@ export default function Editor({
     folders,
     reloadAllNotes
 }: EditorType): React.JSX.Element {
+    const [title, setTitle] = React.useState<string | undefined>('Welcome To My Notes')
+
     if (!activeNote) {
         return (
             <EmptyPage
@@ -44,7 +47,7 @@ export default function Editor({
 
     return (
         <div className="w-full h-full text-light-primaryText dark:text-dark-primaryText flex-1">
-            <div className={`flex flex-row items-center gap-4 justify-end px-6`}>
+            <div className={`flex flex-row items-center gap-4 justify-end px-10 py-6`}>
                 <div className={`flex flex-row items-center gap-2`}>
                     <span
                         className={`text-[10px] font-medium uppercase tracking-wider text-light-secondaryText dark:text-dark-tertext-light-secondaryText`}
@@ -69,7 +72,24 @@ export default function Editor({
                     anchor="top end"
                 />
             </div>
-            <div className="px-12">{activeNote.title}</div>
+            <TextareaAutosize
+                placeholder="Untitled Note"
+                className="mt-16 flex-1 px-12 xl:px-24 w-full wrap-break-word text-2xl lg:text-3xl font-extrabold border-none outline-none bg-transparent placeholder-opacity-40 resize-none"
+                value={title}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        e.preventDefault()
+                    }
+                }}
+                onChange={(e) => {
+                    setTitle(e.target.value)
+                }}
+            />
+            <p
+                className={`mt-10 px-12 xl:px-24 w-full wrap-break-word outline-none text-lg leading-relaxed`}
+            >
+                {activeNote.body}
+            </p>
         </div>
     )
 }
